50aa4083b3fa8691007bba415ed8c0ffbc4db69d
bruce <johnnybgoode@rogers.com>
2015-03-06 17:51:58 -0500

    not working just yet

---
+++ marbles.cpp
@@ -0,0 +1,50 @@
+// marbles.cpp /////////////////////////////////////////////////////////////////
+// testing a shader to draw nice planets in 2d without /////////////////////////
+// crazy opengl work ///////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////
+//#include <std_files>
+//#include "Headers.h"
+
+//#include "Source.c"
+
+//#include "Headers.hpp"
+//#include "Source.cpp"
+#include <iostream>
+#include <SFML/Window.hpp>
+#include <SFML/Graphics.hpp>
+#include <SFML/OpenGL.hpp>
+
+int main()
+{
+    sf::RenderWindow window(sf::VideoMode(800, 800), "SFML works!");
+	sf::Texture planet_tex;
+	if(!planet_tex.loadFromFile("./earth.png"))
+	{	std::cout << "Unable to load planet png file" << std::endl;
+	}
+	sf::Sprite planet_sprite;
+	planet_sprite.setTexture(planet_tex);
+	planet_sprite.setPosition(sf::Vector2f(200,200));
+	
+	 sf::Shader shader;
+
+	// load only the vertex shader
+	if(!shader.loadFromFile("./awesome.vert", "./awesome.frag"))
+	{	std::cout << "Unable to load shaders" << std::endl;
+		// error...
+	}
+
+    while (window.isOpen())
+    {	sf::Event event;
+		while (window.pollEvent(event))
+		{	if (event.type == sf::Event::Closed)
+            {	window.close();
+			}
+		}
+		window.clear();
+		window.draw(planet_sprite, &shader);
+		window.display();
+    }
+    return 0;
+}
+
+
